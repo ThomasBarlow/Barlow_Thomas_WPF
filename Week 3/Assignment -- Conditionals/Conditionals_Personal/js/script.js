020a4284fc76b47eed9693c0fdc7c2ab629e075c
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-21 19:21:39 -0400

    Set up initial "Personal" conditional files

---
+++ Week 3/Assignment -- Conditionals/Conditionals_Personal/js/script.js
@@ -0,0 +1,14 @@
+/*
+
+ Thomas Barlow
+ Tuesday, May 21th 2014
+ Assignment: Conditionals -- Personal
+
+*/
+
+//  This script will check to see weather I have work in the morning and I need to set an alarm clock.
+//  If I do have work it will let me know what time I should set the alarm to.  It will know how much
+//  time I need to get ready from a prompt asking so.
+
+//  questions will be:  Do you have work tomorrow?  What time do you have work? How much time do you need to get ready?
+
