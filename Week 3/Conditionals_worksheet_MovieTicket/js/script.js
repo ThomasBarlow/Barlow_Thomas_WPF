b3784ce3aa6f5858b13a8c324e609248d6a866db
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-20 20:05:19 -0400

    Week 3 - Conditionals Worksheet practice work
    file cleanup

--- Week 3/Conditionals_worksheet_MovieTicket/js/script.js
+++ Week 3/Conditionals_worksheet_MovieTicket/js/script.js
@@ -8,18 +8,6 @@
 **********************/
 
 /*
-    Undergraduate Grade Scale
-    -------------------------
-    A+  =   95 - 100
-    A   =   90 - 94
-    B+  =   85 - 89
-    B   =   80 - 84
-    C+  =   76 - 79
-    C   =   73 - 75
-    D   =   70 - 72
-    F   =   0  - 69
-
-    This short script will give the user a grade letter for the given number
-    percentages entered into the prompt.
+    
 */
 
