907d728638cf6cd69bfb0ead44fae53c7106bd05
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-22 22:19:22 -0400

    Created wacky conditional files
    edit to wacky conditional file

--- Week 3/Assignment -- Conditionals/Conditionals_Wacky/js/script.js
+++ Week 3/Assignment -- Conditionals/Conditionals_Wacky/js/script.js
@@ -6,3 +6,23 @@ Assignment: Conditionals -- Wacky
 
 */
 
+//  This small script will tell you how many of a certain type of fish are in a fish bowl
+//  It will ask if they would like to find fish, if not it will thank them and exit
+
+var fish = new Array();
+
+fish[0] = 'tuna';
+fish[1] = 'salmon';
+fish[2] = 'tuna';
+fish[3] = 'salmon';
+fish[4] = 'marlin';
+fish[5] = 'carp';
+fish[6] = 'marlin';
+
+var fishCount = '';
+var findFish = '';
+var fishType = '';
+
+
+
+
