faf02a8b58a51963f99307d6e0278af50ed65d9a
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-29 11:46:28 -0400

    week 4 video files

---
+++ Week 4/Screencast Follow Along/Basic Function Structure/js/script.js
@@ -0,0 +1,14 @@
+/*
+Basic Function Structure
+ */
+
+function outputMsg() {
+    console.log("Hello World");
+}
+
+function calcArea() {
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
