d24ed80e6404decb4a31d391ebf1b7efa266e960
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-29 17:23:29 -0400

    Cleaned up directory and started Stung file/folder

---
+++ Week 4/Function_Worksheet/Stung/js/script.js
@@ -0,0 +1,8 @@
+//  Thomas Barlow
+//  Thursday, May 29
+//  Function Worksheet
+
+/*
+    It takes 8.666666667 bee stings per pound to kill an animal
+    Calculate how many bee stings are needed to kill an animal in a function.
+*/
