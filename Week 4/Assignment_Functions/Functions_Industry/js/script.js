a209831736b7cb6eabc4766d3f519eb0470c6a80
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-29 22:55:30 -0400

    industry function setup

--- Week 4/Assignment_Functions/Functions_Industry/js/script.js
+++ Week 4/Assignment_Functions/Functions_Industry/js/script.js
@@ -3,3 +3,21 @@
     May 29, 2014
     Assignment - Functions_Industry
  */
+
+//  This function will tell you how many boxwood plants you will need in order
+//  to plant the perimeter of a specific garden
+
+//  to find the perimeter of a square the math is length + length + height + height
+
+//  variable to hold length
+var gardenLength = prompt("What is the length: ");
+//  variable to hold height
+var gardenHeight = prompt("What is the height: ");
+//  variable asking how wide boxwoods are
+var boxwoodWidth = prompt("How wide are the boxwoods: ")
+
+//  anonymous function returning perimeter
+var gardenPerimeter = function(l, h){
+    perimeter = l + l + h + h;
+    return perimeter;
+}
