55341370e46d875dd1a1085f34e72649a2848a8a
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-29 23:51:34 -0400

    cleaned up the industrial code and commented

--- Week 4/Assignment_Functions/Functions_Industry/js/script.js
+++ Week 4/Assignment_Functions/Functions_Industry/js/script.js
@@ -10,15 +10,32 @@
 //  to find the perimeter of a square the math is length + length + height + height
 
 //  variable to hold length
-var gardenLength = prompt("What is the length: ");
+var gardenLength = prompt("What is the garden length: ");
+//  validation check for input
+while(isNaN(gardenLength) || gardenLength === ""){
+    gardenLength = prompt("Please enter the garden LENGTH: ");
+}
 //  variable to hold height
-var gardenHeight = prompt("What is the height: ");
+var gardenHeight = prompt("What is the garden height: ");
+//  validation check for input
+while(isNaN(gardenHeight) || gardenHeight === ""){
+    gardenHeight = prompt("Please enter the garden HEIGHT: ")
+}
 //  variable asking how wide boxwoods are
 var boxwoodWidth = prompt("How wide are the boxwoods: ");
+//  input validation
+while(isNaN(boxwoodWidth) || boxwoodWidth === ""){
+    boxwoodWidth = prompt("Please enter the individual boxwood WIDTH: ");
+}
 
+//  converts gardenHeight to a number for calculations
 gardenHeight = Number(gardenHeight);
+// converts gardenLength to a number for calculations
 gardenLength = Number(gardenLength);
+// converts boxwoodWidth to a number for calculations
 boxwoodWidth = Number(boxwoodWidth);
+
+//  function invocation
 var plantArea = gardenPerimeter(gardenLength, gardenHeight);
 
 //function returning perimeter
@@ -27,8 +44,12 @@ function gardenPerimeter(l, h){
     return perimeter;
 }
 
+//  anonymous function making the final calculations of the program
 var finalSolution = function(){
     return plantArea / boxwoodWidth;
 };
 
 console.log("You need to buy " + finalSolution() + " boxwood trees!");
+
+//  There were probably many other ways to do this, but it was the best way I could
+//  find to use an anonymous function for the credit
