250c6fb537e1ea85cfec97e4877c9cddfec9f8a1
Thomas Barlow <tbarlow@fullsail.edu>
2014-05-29 22:45:55 -0400

    Personal function set up and tested

--- Week 4/Assignment_Functions/Functions_Personal/js/script.js
+++ Week 4/Assignment_Functions/Functions_Personal/js/script.js
@@ -3,3 +3,46 @@
     May 29, 2014
     Assignment - Functions_Personal
  */
+
+//  This short script will evaluate weather you should buy a seasonal beach badge
+//  or simply purchase a daily one instead
+
+
+//  variable asking how many days are left in the beach season
+var daysOfBeachLeft = prompt("How many days are left in the beach season?");
+//  while loop checking for proper input of variable
+while(isNaN(daysOfBeachLeft)){
+    var daysOfBeachLeft = prompt("Please enter a NUMBER");
+}
+//  variable asking daily badge price
+var dailyBadgePrice = prompt("How much is a DAILY badge?");
+//  while loop for validation
+while(isNaN(dailyBadgePrice)) {
+    var dailyBadgePrice = prompt("Please enter a NUMBER");
+}
+//  variable asking seasonal badge price
+var seasonalBadgePrice = prompt("How much is a SEASONAL badge?");
+//  while loop for validation
+while(isNaN(seasonalBadgePrice)){
+    var seasonalBadgePrice = prompt("Please enter a NUMBER");
+}
+
+var answer = buySeasonalBadge(daysOfBeachLeft, dailyBadgePrice, seasonalBadgePrice);
+
+function buySeasonalBadge(days, dayPrice, seasonPrice){
+    var buyTicket = "";
+    if (days * dayPrice > seasonPrice){
+        console.log("You should think of buying a seasonal ticket")
+        buyTicket = "Buy a seasonal ticket";
+    } else if (days * dayPrice == seasonPrice){
+        console.log("It doesn't matter, you will brake even.");
+        buyTicket = "Either way";
+    } else if (days * dayPrice < seasonPrice){
+        console.log("You don't need a seasonal ticket")
+        buyTicket = "You don't need a seasonal ticket";
+    }
+    return buyTicket;
+}
+
+
+console.log(answer);
